0533213ac247dbeb4a145ba3956bef5bfc997c11
arielatom-git <coolguy706@gmail.com>
2022-02-09 11:00:02 -0800

    Solved excercieses 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,14 +1,14 @@
 const repeatString = function(string, number) {
     let repeat = ""
-    for (let i = 0; i < number; i++) {
-        if (number >= 0) {
+    if (number >= 0) {
+        for (let i = 0; i < number; i++) {
             repeat += string
-        } else {
-            return "hello"
         }
+    } else {
+        return "ERROR"
     }
-    // return repeat
+    return repeat
 };
-
+console.log(repeatString("hey", -1))
 // Do not edit below this line
 module.exports = repeatString;
