c6991281f99aec4096929b4f0f43bc6ec4596e8b
arielatom-git <coolguy706@gmail.com>
2022-02-09 10:14:10 -0800

    Test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let repeat = ""
+    for (let i = 0; i < number; i++) {
+        if (number >= 0) {
+            repeat += string
+        } else {
+            return "hello"
+        }
+    }
+    // return repeat
 };
 
 // Do not edit below this line
