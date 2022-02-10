d5bd9d0da84918b829b7deeb6d4d09bf12f3bf0a
arielatom-git <coolguy706@gmail.com>
2022-02-10 01:33:50 -0800

    Removed 'else' block

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,9 +3,7 @@ const removeFromArray = function(arr, ...args) {
         if (arr.indexOf(arg) >= 0) {
             let arrIndex = arr.indexOf(arg)
             arr.splice(arrIndex, 1)
-        } else {
-            void(0)
-        }
+        } 
     }
     return arr
 };
